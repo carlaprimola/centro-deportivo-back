7260f3921c13b5a4262468363463823589aec118
Carla Escobar <carlaprimola@outlook.es>
2024-05-09 12:15:41 +0200

    backend_users_aunthentication02

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
-//importamos express
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-
-import authRoutes from './routes/auth.routes.js'
-import taskRoutes from './routes/task.routes.js'
+import connectDB from './db.js';
+import authRoutes from './routes/auth.routes.js';
+import taskRoutes from './routes/task.routes.js';
 
 const app = express();
 
-//configuramos el puerto
+// Configura el puerto
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
 }));
 
-app.use(morgan('dev')); //simplifica el proceso de registros
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 
+// Conecta a la base de datos antes de definir las rutas
+connectDB().then(() => {
+  // Define la ruta para obtener todos los usuarios
+  app.get('/api/users', async (req, res) => {
+    try {
+      // Consulta la base de datos para obtener todos los usuarios
+      const users = await User.find();
 
-app.use('/', (req, res) =>{
-    res.json({message: 'Bienvenido a la API de Proyectos'})
-})
-//añadimos /api antes de la ruta para distinguirlo de las rutas del front
-app.use("/api", authRoutes) 
-app.use("/api", taskRoutes)
+      // Devuelve los usuarios como respuesta
+      res.json(users);
+    } catch (error) {
+      // Maneja cualquier error que ocurra
+      res.status(500).json({ error: 'Error al obtener los usuarios' });
+    }
+  });
+
+  // Define la ruta de bienvenida
+  app.use('/', (req, res) =>{
+    res.json({message: 'Bienvenido a la API de Centro Deportivo Ciudad Los Ángeles'})
+  });
+
+  // Añade las rutas de autenticación y tareas
+  app.use("/api", authRoutes);
+  app.use("/api", taskRoutes);
+
+  // Inicia el servidor
+  app.listen(3000, () => {
+    console.log('✨ Servidor conectado', 3000);
+  });
+}).catch((error) => {
+  console.error('❌ Error al iniciar el servidor:', error);
+});
 
 export default app;
--- db.js
+++ db.js
@@ -1,19 +1,14 @@
 import mongoose from "mongoose";
-import path from 'path';
-import app from './app.js';
-import express from "express";
 
 const connectDB = async () => {
   try {
-    //await mongoose.connect("mongodb+srv://carla:123@cluster0.yxcgqr7.mongodb.net/todo")
-    await mongoose.connect("mongodb+srv://sudosu:settym-surzuv-2fuQqo@free-cluster.zgdlrwr.mongodb.net/centro_deportivo")
+    const connection = await mongoose.connect("mongodb+srv://sudosu:settym-surzuv-2fuQqo@free-cluster.zgdlrwr.mongodb.net/centro_deportivo");
     console.log("🎉 Conectado a MongoDB");
+    return connection;
   } catch (error) {
     console.log("❌ Error al conectar a MongoDB", error);
+    throw error;
   }
 };
 
 export default connectDB;
-
-
-
