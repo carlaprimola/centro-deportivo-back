6310facf0e9000bf42efd42f5644affe57ccf12b
Lea Montoya <leamontoya19@gmail.com>
2024-05-21 09:59:21 +0200

    backend_develop_updated02

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 import express from "express";
 /* import morgan from 'morgan'; */
-/* import cookieParser from 'cookie-parser'; */
 import cors from 'cors';
 import authRoutes from './routes/auth.routes.js';
 import memberPaymentRouter from './routes/memberPayment.routes.js'
-
-/* import authRoutes from './routes/auth.routes.js'*/
-
 
 import cookieParser from 'cookie-parser';
 import ProductRouter from "./routes/product.routes.js";
@@ -20,6 +16,7 @@ app.use(cors());
 
 app.use(cors({
     origin:'http://localhost:5173',
+    credentials: true, //esto es lo que permite credenciales (cookies, autorizaciones)
 }));
 
 app.use(express.json());
@@ -28,6 +25,7 @@ app.use(cookieParser());
 /* app.use('/', (req, res) =>{
     res.json({message: 'Bienvenido a la API de Proyectos'})
 }) */
+
 //añadimos /api antes de la ruta para distinguirlo de las rutas del front
 app.use("/api/payments", memberPaymentRouter) ;
 app.use("/api/", authRoutes)
--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -5,12 +5,13 @@ import { TOKEN_SECRET } from '../config.js';
 export const authRequired = async (req, res, next) => {
     
     //Si hay token puedes continuar, sino te bloquea
-    const {token} = req.cookies;
-    
+    //const {token} = req.cookies;
+    const token = req.cookies.token || req.headers['authorization'];
+
     if (!token) 
         return res.status(401).json({message: 'Acceso no autorizado'});
 
-        jwt.verify(token, TOKEN_SECRET,(err, user, name) => {
+        jwt.verify(token, TOKEN_SECRET,(err, user) => {
             if(err) return res.status(403).json({message: 'El token no es válido'});
 
             req.user = { _id: user._id } //Aquí asigno el id del usuario authenticado a req.user
