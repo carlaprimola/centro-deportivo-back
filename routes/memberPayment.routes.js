6adabca5836dda242ea90a4bf224feae559b58dc
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2024-05-27 11:14:12 +0200

    backend_feature/user-pàyments_fixedPaymentStatusBug

--- routes/memberPayment.routes.js
+++ routes/memberPayment.routes.js
@@ -5,11 +5,11 @@ import { isAdmin } from "../controllers/auth.controller.js";
 import { verifyToken } from "../controllers/auth.controller.js";
 const router = Router();
 
-router.get('/', verifyToken, getAllMembershipPayments);
+router.get('/', verifyToken, isAdmin, getAllMembershipPayments);
 router.get('/payment/:id',  verifyToken,  getSingleMembershipPayment);
 router.post('/payment/',  verifyToken, createMembershipPayment);
 router.put('/payment/:id',  verifyToken, updateMembershipPayment);
-router.put('/payment/status/:id',  verifyToken, isAdmin ,updatePaymentStatus);
+router.put('/status/:id',  verifyToken, isAdmin ,updatePaymentStatus);
 router.delete('/payment/:id',  verifyToken, deleteMembershipPayment);
 // router.delete('/kcali/:id/:paymentType', deleteSinglePayment);
 
