bb4d0c7fad0c40a341d0e9bd06fb6aefb1d3e283
andreagarciacedeno <andreagarciacedeno@gmail.com>
2024-05-28 12:16:53 +0200

    backend_feature/orders-users_conection_GetAllOrdersAdmin_back&front

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import { authRequired } from '../middlewares/validateToken.js';
 import { getMyOrders } from '../controllers/users.orders.controller.js';
 import OrderController from '../controllers/orders.controller.js';
+import { isAdmin, verifyToken } from '../controllers/auth.controller.js';
 
 
 const OrderRouter = Router();
 
 OrderRouter
-    .get('/', OrderController.getAllOrders)
+    .get('/', verifyToken, isAdmin, OrderController.getAllOrders)
     .get('/order/:id', OrderController.getOrderById)
     .post('/add-order', OrderController.addOrder)
     .put('/order/:id', OrderController.updateOrder)
