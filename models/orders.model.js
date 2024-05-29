aa133211d0665a4780e746c0e6a78a7d10aaba8d
Carla Escobar <carlaprimola@outlook.es>
2024-05-29 09:14:25 +0200

    backend_develop_addedPDFInputOrders01

--- models/orders.model.js
+++ models/orders.model.js
@@ -14,7 +14,6 @@ const orderSchema = new mongoose.Schema({
     }],
     summary: {
         type: String,
-
     },
     status: {
         type: String,
@@ -22,7 +21,8 @@ const orderSchema = new mongoose.Schema({
         default: 'pendiente'
     },
     document: {
-        type: String
+        data: Buffer, // Campo para almacenar el archivo adjunto
+        contentType: String // Tipo de contenido del archivo adjunto
     }
 }, {
     timestamps: true,
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -3,14 +3,25 @@ import { authRequired } from '../middlewares/validateToken.js';
 import { getMyOrders } from '../controllers/users.orders.controller.js';
 import OrderController from '../controllers/orders.controller.js';
 import { verifyToken } from '../controllers/auth.controller.js';
+import multer from 'multer';
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage: storage }).single('document');
 
 const OrderRouter = Router();
 
 OrderRouter
     .get('/', OrderController.getAllOrders)
     .get('/order/:id', OrderController.getOrderById)
-    .post('/add-order', OrderController.addOrder)
+    .post('/add-order',upload, OrderController.addOrder)
     .put('/order/:id', OrderController.updateOrder)
     .delete('/order/:id', OrderController.deleteOrder)
     .get('/myorders', verifyToken, authRequired, getMyOrders)
