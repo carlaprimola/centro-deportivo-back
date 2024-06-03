31fe9a25b807c56a28334755e6e4d1f42130757a
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2024-06-03 10:27:33 +0200

    backend_develop-fixed-mypayments-status-bug

--- routes/memberPayment.routes.js
+++ routes/memberPayment.routes.js
@@ -1,16 +1,26 @@
 import { Router } from "express";
-import { createMembershipPayment, deleteMembershipPayment, getAllMembershipPayments, getSingleMembershipPayment, updateMembershipPayment, updatePaymentStatus } from "../controllers/memberPayment.controller.js";
+import { createMembershipPayment, deleteMembershipPayment, getAllMembershipPayments, getMyPaymentStatus, getSingleMembershipPayment, updateMembershipPayment, updatePaymentStatus } from "../controllers/memberPayment.controller.js";
 import { isAdmin } from "../controllers/auth.controller.js";
 
 import { verifyToken } from "../controllers/auth.controller.js";
 const router = Router();
 
 router.get('/', verifyToken, isAdmin, getAllMembershipPayments);
+
 router.get('/payment/:id',  verifyToken,  getSingleMembershipPayment);
+
+router.get('/my-status/',  verifyToken,  getMyPaymentStatus);
+
 router.post('/payment/',  verifyToken, createMembershipPayment);
+
 router.put('/payment/:id',  verifyToken, updateMembershipPayment);
-router.put('/status/:id',  verifyToken, isAdmin ,updatePaymentStatus);
+
+
 router.delete('/payment/:id',  verifyToken, deleteMembershipPayment);
+
+
+router.put('/status/:id',  verifyToken, isAdmin ,updatePaymentStatus);
+
 // router.delete('/kcali/:id/:paymentType', deleteSinglePayment);
 
 export default router;
