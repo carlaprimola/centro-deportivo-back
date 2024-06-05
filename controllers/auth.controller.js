50c91879fee72e766074d0e355f2c38e48bf0ccf
Carla Escobar <carlaprimola@outlook.es>
2024-06-05 09:21:31 +0200

    backend_products_issueFixedNewOrder02

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -146,7 +146,12 @@ export const profile = async (req, res) => {
 
 // Verificación de Token
 export const verifyToken = async (req, res, next) => {
-    const token  = req.headers.authorization.split(" ")[1];
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ message: "No se ha encontrado ningún token en el encabezado de autorización" });
+    }
+    const token = authHeader.split(" ")[1];
+    // const token  = req.headers.authorization.split(" ")[1];
     console.log(req.headers.authorization)
     console.log(token)
     console.log("🔐 Cookies:", token);
