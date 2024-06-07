e242bd3d9f1372d496dd9abbda4364d284697d36
andreagarciacedeno <andreagarciacedeno@gmail.com>
2024-06-07 10:04:42 +0200

    backend_deploy_update_valide_token

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -9,15 +9,24 @@ export const authRequired = async (req, res, next) => {
     const role = req.body
     //console.log(`Rol del usuario autenticado: ${role}`);
 
-  if (!token) return res.status(401).json({ message: "Acceso no autorizado" });
+//   if (!token) return res.status(401).json({ message: "Acceso no autorizado" });
 
-        jwt.verify(token, TOKEN_SECRET,(err, user) => {
-            if (err) {
-                return res.status(403).json({ message: 'El token no es válido' });
-            }
+//         jwt.verify(token, TOKEN_SECRET,(err, user) => {
+//             if (err) {
+//                 return res.status(403).json({ message: 'El token no es válido' });
+//             }
 
-            req.user = { _id: user._id, role: user.role } //Aquí asigno el id del usuario authenticado a req.user
-            next();
-        })
-   
+//             req.user = { _id: user._id, role: user.role } //Aquí asigno el id del usuario authenticado a req.user
+//             next();
+//         })
+if (!token) return res.status(401).json({ message: "Acceso no autorizado" });
+
+    try {
+        const payload = jwt.verify(token, TOKEN_SECRET);
+        req.user = { _id: payload._id, rol_id: payload.role }; // Aquí asigno el id del usuario authenticado a req.user
+        next();
+    } catch (err) {
+        return res.status(403).json({ message: 'El token no es válido' });
+    }
 }
+   
