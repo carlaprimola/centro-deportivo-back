276ba746c1a6f61e62ea2d91a16f153958a265af
andreagarciacedeno <andreagarciacedeno@gmail.com>
2024-06-07 18:25:07 +0200

    backend_deploy_added_authrequired_assign-team

--- routes/teams.routes.js
+++ routes/teams.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { assignTeamToPlayer } from '../controllers/teams.controller.js';
 import { getSingleTeam, getAllTeams } from '../controllers/teams.controller.js';
+import { authRequired } from '../middlewares/validateToken.js';
 
 const router = express.Router();
 
-router.post('/assign-team', assignTeamToPlayer);
+router.post('/assign-team', authRequired, assignTeamToPlayer);
 router.get('/team/:id', getSingleTeam);
 router.get('/teams', getAllTeams);
 
